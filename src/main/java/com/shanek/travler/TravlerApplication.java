8d7564b16e86b9df64a5c9e92802b3d6bdba417d
Shane Kaestner <shane.kaestner@yahoo.com>
2020-09-28 20:27:13 -0700

    initial commit

---
+++ src/main/java/com/shanek/travler/TravlerApplication.java
@@ -0,0 +1,14 @@
+package com.shanek.travler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravlerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravlerApplication.class,
+            args);
+    }
+
+}
---
+++ src/test/java/com/shanek/travler/TravlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shanek.travler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravlerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
